76ddb4b2da782fb9d79a5d5901870432acdda73c
yananob <yananob@gmail.com>
2026-04-19 11:45:07 +0900

    update

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 set -eu
 
-# 静的解析を実行
-echo "Running PHPStan..."
-./vendor/bin/phpstan analyze -c phpstan.neon
+source ./tests/secrets.sh
+source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
 
-if [ -f "./tests/secrets.sh" ]; then
-    echo "Exporting secrets..."
-    source ./tests/secrets.sh
-    if [ -d "./_cf-common/test" ]; then
-        source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
-    fi
-fi
+# run tests
+bash ./tests/run_linter.sh
 
-# PHPUnitを実行
 echo "Running PHPUnit..."
-./vendor/bin/phpunit tests/
+./vendor/bin/phpunit
 
-if [ -f "./tests/secrets.sh" ] && [ -d "./_cf-common/test" ]; then
-    source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
-fi
+source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
