5769b0712b66a9b7efab2574144a39f8a1f67577
yananob <yananob@gmail.com>
2026-08-28 19:47:40 +0900

    creds

--- src/Service/GmailClientFactory.php
+++ src/Service/GmailClientFactory.php
@@ -24,9 +24,8 @@ class GmailClientFactory
         $client = new Client();
         $client->setApplicationName('MyCFApp');
         $client->setScopes([
-            Gmail::MAIL_GOOGLE_COM,
-            Gmail::GMAIL_MODIFY,
-            Gmail::GMAIL_READONLY,
+            Gmail::MAIL_GOOGLE_COM,  // Full access to Gmail
+            Gmail::GMAIL_MODIFY,      // Modify Gmail labels and messages
         ]);
         $client->setAccessType('offline');
 
